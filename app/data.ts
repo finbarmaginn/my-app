cf683471603749256076587cdae0aafb42793bf7
findeva <finbarmaginn@gmail.com>
2025-01-05 14:27:21 +0000

    tv dash

---
+++ app/data.ts
@@ -0,0 +1,32 @@
+export const services = [
+	{
+		name: "Netflix",
+		url: "https://www.netflix.com/browse",
+		img: "netflixlogo.svg",
+	},
+	{
+		name: "Amazon Prime",
+		url: "https://www.amazon.co.uk/gp/video/storefront",
+		img: "/primevideo-logo.png",
+	},
+	{
+		name: "Dinsey+",
+		url: "https://www.disneyplus.com/en-gb/home",
+		img: "disney+logo.png",
+	},
+	{
+		name: "BBC iPlayer",
+		url: "https://www.bbc.co.uk/iplayer",
+		img: `/iplayerlogo.svg`
+	},
+	{
+		name: "Channel 4",
+		url: "https://www.channel4.com/",
+		img: "/all4_logo.svg",
+	},
+	{
+		name: "YouTube",
+		url: "https://www.youtube.com/",
+		img: "/yt-logo.png",
+	},
+]
