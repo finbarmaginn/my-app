fb6357c673a78bd447aa92b05c2b0e0d27778036
finbarfunky <124581801+finbarfunky@users.noreply.github.com>
2025-01-19 13:44:35 +0000

    date clock and new layout

---
+++ app/_components/Clock.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { format } from "date-fns";
+import { useEffect, useRef, useState } from "react";
+
+export default function Clock() {
+  const timer = useRef<NodeJS.Timeout | null>(null);
+  const [time, setTime] = useState<{ date: string, time: string } | null>(null);
+
+  useEffect(() => {
+    timer.current = setInterval(() => {
+      const now = Date.now()
+      setTime({
+        date: format(now, "eeee do LLLL"),
+        time: format(now, "p")
+      })
+    }, 1000)
+    return () => clearInterval(timer.current as NodeJS.Timeout);
+  }, [])
+
+  return (
+    <div>
+
+      <div className="mx-auto my-5 max-w-fit text-center">
+        {!time ? (<>
+          <div className="w-48 h-[76px] bg-neutral-800 animate-pulse"></div>
+        </>) : (<>
+          <div className="lg:text-xl">{time.date}</div>
+          <div className="text-4xl lg:text-5xl">{time.time}</div>
+        </>)}
+      </div>
+
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,34 @@
 import Image from "next/image";
 import { services } from "./data";
+import Clock from "./_components/Clock";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col h-full items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-			<main className="max-w-screen-2xl w-11/12 my-auto">
-				<div className="grid grid-cols-2 md:grid-cols-3 gap-24 items-center justify-center">
-					{services.map((service) => {
-						return (
-							<a className="block transition-opacity duration-300 hover:opacity-40" href={service.url} key={service.name} target="_blank">
-								<Image
-									src={service.img}
-									alt={service.name}
-									width={20}
-									height={20}
-									className="w-full max-h-[30dvh]"
-									unoptimized
-									priority
-								/>
-							</a>
-						)
-					})}
-				</div>
-			</main>
-		</div>
+		<div className="grid grid-cols-12">
+			<div className="col-span-12 md:col-span-2 px-5 bg-neutral-900">
+				<Clock />
+			</div>
+			<div className="col-span-12 md:col-span-10 flex flex-col items-center justify-items-center md:min-h-screen font-[family-name:var(--font-geist-sans)]">
+				<main className="max-w-screen-2xl w-11/12 my-6 md:my-auto">
+					<div className="grid grid-cols-2 md:grid-cols-3 gap-12 lg:gap-24 items-center justify-center">
+						{services.map((service) => {
+							return (
+								<a className="block transition-opacity duration-300 hover:opacity-40" href={service.url} key={service.name} target="_blank">
+									<Image
+										src={service.img}
+										alt={service.name}
+										width={20}
+										height={20}
+										className="w-full max-h-[30dvh]"
+										unoptimized
+										priority
+									/>
+								</a>
+							)
+						})}
+					</div>
+				</main>
+			</div>
+		</div >
 	);
 }
