952d579af1dfdfb4bda6c08228b27c52394d0263
Finbar Maginn <finbarmaginn@gmail.com>
2025-01-19 12:11:52 +0000

    dash changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { services } from "./data";
 export default function Home() {
 	return (
 		<div className="flex flex-col h-full items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-			<main className=" w-11/12 my-auto">
-				<div className="grid grid-cols-3 gap-24 items-center justify-center">
+			<main className="max-w-screen-2xl w-11/12 my-auto">
+				<div className="grid grid-cols-2 md:grid-cols-3 gap-24 items-center justify-center">
 					{services.map((service) => {
 						return (
 							<a className="block transition-opacity duration-300 hover:opacity-40" href={service.url} key={service.name} target="_blank">
